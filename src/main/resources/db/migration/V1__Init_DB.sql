ed38ebacb07fcf86ef8b321395ca297ce604088a
mkanapelka <MKanapelka@piesoft.us>
2022-08-11 17:51:51 +0300

    clean up

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -2,47 +2,45 @@ create sequence hibernate_sequence start 1 increment 1;
 
 create table enter_balance
 (
-    id      bigserial not null,
-    active  double precision,
-    passive double precision,
-    account_id int8,
+    id                 bigserial not null,
+    active             double precision,
+    passive            double precision,
+    account_id         int8,
     summary_balance_id int8,
     primary key (id)
 );
 
 create table money_flow
 (
-    id      bigserial not null,
-    debit  double precision,
-    credit double precision,
-    account_id int8,
+    id                 bigserial not null,
+    debit              double precision,
+    credit             double precision,
+    account_id         int8,
     summary_balance_id int8,
     primary key (id)
 );
 
 create table proceed_balance
 (
-    id      bigserial not null,
-    active  double precision,
-    passive double precision,
-    account_id int8,
+    id                 bigserial not null,
+    active             double precision,
+    passive            double precision,
+    account_id         int8,
     summary_balance_id int8,
     primary key (id)
 );
 
 create table summary_balance
 (
-    id      bigserial not null,
-    active  double precision,
-    passive double precision,
-    file_id int8,
+    id         bigserial not null,
+    file_id    int8,
     account_id int8,
     primary key (id)
 );
 
 create table file
 (
-    id      bigserial not null,
+    id        bigserial not null,
     file_name varchar(255),
     primary key (id)
 );
