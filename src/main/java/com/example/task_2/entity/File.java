7bc3cc0bf59c62d3df490bc2d94fe229947d3010
mkanapelka <MKanapelka@piesoft.us>
2022-08-11 17:29:10 +0300

    add fields with date in entity File

--- src/main/java/com/example/task_2/entity/File.java
+++ src/main/java/com/example/task_2/entity/File.java
@@ -14,6 +14,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
@@ -27,6 +29,10 @@ public class File {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String fileName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
     @OneToMany(mappedBy = "file", fetch = FetchType.LAZY)
     private List<EnterBalance> enterBalances;
 
@@ -36,5 +42,4 @@ public class File {
     @OneToMany(mappedBy = "file", fetch = FetchType.LAZY)
     private List<MoneyFlow> moneyFlows;
 
-    private String fileName;
 }
